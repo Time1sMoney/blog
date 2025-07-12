45a42503188ddb5b5d5a5c01bbd3b376c470f5ca
chenbin.li <1015761882@qq.com>
2025-07-12 16:59:33 +0800

    fix: correct collection name

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,16 +2,16 @@ import rss from '@astrojs/rss';
 import type { AstroConfig } from 'astro';
 import { getCollection } from 'astro:content';
 export async function GET(context: AstroConfig) {
-  const blogs = await getCollection('blogs');
+  const blogs = await getCollection('blog');
   return rss({
     title: "Codercoin's Blog",
     description: 'This is a tech blog written by codercin. ',
     site: context.site!,
-    items: blogs.map(({ data, slug }) => ({
+    items: blogs.map(({ data }) => ({
       title: data.title,
       pubDate: data.pubDateTime,
       description: data.description,
-      link: `/blog/${slug}/`
+      link: `/blog/${data.postSlug}/`
     }))
   });
 }
